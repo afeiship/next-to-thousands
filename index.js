1d7e8b9da201938bcf847caa001a2db202789929
feizheng <1290657123@qq.com>
2016-12-26 17:21:25 +0800

    bugfix

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var pointRe = /^(\d+)((\.\d+)?)$/;
 module.exports = function(inString, inSpeparator) {
   var replaceStr = '$&' + (inSpeparator || ',');
   var numberVal = inString * 1;
-  var string = numberVal>0 ? String(inString) : String(inString).slice(1);
+  var string = numberVal>=0 ? String(inString) : String(inString).slice(1);
   var value = string.replace(pointRe, function(str, s1, s2) {
     return s1.replace(thousandsRE, replaceStr) + s2;
   });　
-  return numberVal > 0 ? value : '-' + value;
+  return numberVal >= 0 ? value : '-' + value;
 };
