8f7df24b6fd038e805fb72ff0c8ad160eab95088
feizheng <1290657123@qq.com>
2016-12-26 16:35:42 +0800

    add test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var thousandsRE = /(\d)(?=(\d{3})+$)/g;
+var thousandsRE = /\d{1,3}(?=(\d{3})+$)/g;
+var pointRe = /^(\d+)((\.\d+)?)$/;
 
-module.exports = function(inString,inSpeparator){
-  var replaceStr = '$1' + (inSpeparator || ',');
-  return String(inString).replace(thousandsRE, replaceStr);
+module.exports = function(inString, inSpeparator) {
+  var replaceStr = '$&' + (inSpeparator || ',');
+  var numberVal = inString * 1;
+  var value = String(Math.abs(numberVal)).replace(pointRe, function(str, s1, s2) {
+    return s1.replace(thousandsRE, replaceStr) + s2;
+  });　
+  return numberVal > 0 ? value : '-' + value;
 };
---
+++ test/test.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var toThousands = require('../index');
+describe('toThousands test main', function() {
+
+  describe('+number <=3', function() {
+    it("toThousands(10),'10'", function() {
+      assert.equal( toThousands(10),'10');
+    });
+    it("toThousands(100),'100'", function() {
+      assert.equal( toThousands(100),'100');
+    });
+  });
+
+
+  describe('+number >3', function() {
+    it("toThousands(1000),'1,000'", function() {
+      assert.equal( toThousands(1000),'1,000');
+    });
+    it("toThousands(10000),'10,000'", function() {
+      assert.equal( toThousands(10000),'10,000');
+    });
+  });
+
+  describe('+number with float(point)', function() {
+    it("toThousands(1000.123),'1,000.123'", function() {
+      assert.equal( toThousands(1000.123),'1,000.123');
+    });
+    it("toThousands(10000.123),'10,000.123'", function() {
+      assert.equal( toThousands(10000.123),'10,000.123');
+    });
+  });
+
+
+  describe('-number with float(point) <=3', function() {
+    it("toThousands(-100.123),'-100.123'", function() {
+      assert.equal( toThousands(-100.123),'-100.123');
+    });
+    it("toThousands(-100.123),'-100.123'", function() {
+      assert.equal( toThousands(-1000.123),'-1,000.123');
+    });
+  });
+
+
+  describe('-number with float(point) >3', function() {
+    it("toThousands(-12341400.123),'-12,341,400.123'", function() {
+      assert.equal( toThousands(-12341400.123),'-12,341,400.123');
+    });
+  });
+
+});
